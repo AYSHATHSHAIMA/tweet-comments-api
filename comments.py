4650bfb2a70e57191ad46615dcddd2f1c3b2ded5
Ayshath_Shaima <ayshathshaima119@gmail.com>
2025-11-16 21:19:26 -0800

    Add gunicorn for Render deployment

--- comments.py
+++ comments.py
@@ -1,10 +1,10 @@
 comments = [
-    {"id": 1, "text": "Great job! This is amazing!", "author": "@user1", "timestamp": "2m ago"},
-    {"id": 2, "text": "Love it!", "author": "@user2", "timestamp": "5m ago"},
-    {"id": 3, "text": "This is absolutely fantastic work! Keep it up!", "author": "@user3", "timestamp": "10m ago"},
-    {"id": 4, "text": "Not good", "author": "@user4", "timestamp": "15m ago"},
-    {"id": 5, "text": "Awesome work, keep going and never stop creating!", "author": "@user5", "timestamp": "20m ago"},
-    {"id": 6, "text": "Nice", "author": "@user6", "timestamp": "25m ago"},
-    {"id": 7, "text": "This needs significant improvement in many areas.", "author": "@user7", "timestamp": "30m ago"},
-    {"id": 8, "text": "👍", "author": "@user8", "timestamp": "35m ago"}
+    {"id": 1, "text": "Great job! This is amazing!", "author": "@Vinay", "timestamp": "2m ago"},
+    {"id": 2, "text": "Love it!", "author": "@shree", "timestamp": "5m ago"},
+    {"id": 3, "text": "This is absolutely fantastic work! Keep it up!", "author": "@Anu", "timestamp": "10m ago"},
+    {"id": 4, "text": "Not good", "author": "@Fathima", "timestamp": "15m ago"},
+    {"id": 5, "text": "Awesome work, keep going and never stop creating!", "author": "@Roshni", "timestamp": "20m ago"},
+    {"id": 6, "text": "Nice", "author": "@Yadhu", "timestamp": "25m ago"},
+    {"id": 7, "text": "This needs significant improvement in many areas.", "author": "@Midhun", "timestamp": "30m ago"},
+    {"id": 8, "text": "👍", "author": "@Akasha", "timestamp": "35m ago"}
 ]
